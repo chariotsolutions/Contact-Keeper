ceca4d184a8c9eac38983b15e5196f8e201cca1d
Steven Beyers <sbeyers@chariotsolutions.com>
2017-06-06 08:16:53 -0400

    Complete basic app - including details view

--- Contact Keeper/DetailViewController.swift
+++ Contact Keeper/DetailViewController.swift
@@ -10,13 +10,34 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var phoneNumberLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var addrLineOneLabel: UILabel!
+    @IBOutlet weak var addrLineTwoLabel: UILabel!
+    @IBOutlet weak var callButton: UIButton!
+    @IBOutlet weak var sendTextButton: UIButton!
+    @IBOutlet weak var sendEmailButton: UIButton!
 
-
+    @IBOutlet var hidableViews: [UIView]!
+    
     func configureView() {
         // Update the user interface for the detail item.
-        if let contact = contact, let label = detailDescriptionLabel {
-            label.text = contact.firstName
+        let shouldHide = contact == nil
+        _ = hidableViews?.map { $0.isHidden = shouldHide }
+        
+        if let contact = contact {
+            self.title = contact.firstName
+            
+            imageView?.image = contact.image
+            nameLabel?.text = contact.fullName
+            phoneNumberLabel?.text = contact.phoneNumber
+            emailLabel?.text = contact.emailAddress
+            addrLineOneLabel?.text = contact.homeAddress.streetAddress
+            addrLineTwoLabel?.text = "\(contact.homeAddress.city), \(contact.homeAddress.state) \(contact.homeAddress.zip)"
+        } else {
+            self.title = "Select A Contact"
         }
     }
 
@@ -38,6 +59,17 @@ class DetailViewController: UIViewController {
         }
     }
 
+    @IBAction func callPresed(_ sender: Any) {
+        print("Call \(contact?.firstName ?? "contact")")
+    }
 
+    @IBAction func sendTextPressed(_ sender: Any) {
+        print("Send text to \(contact?.firstName ?? "contact")")
+    }
+    
+    @IBAction func sendEmailPressed(_ sender: Any) {
+        print("Send email to \(contact?.firstName ?? "contact")")
+    }
+    
 }
 
